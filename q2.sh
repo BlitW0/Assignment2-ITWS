143ad9377bb1bba8109651445e0d3d8854f93c31
Divyesh Jain <divyjain567@gmail.com>
2017-10-09 13:15:04 +0000

    Upload New File

---
+++ q2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cnt=0
+ls | while read b
+do
+	if [ -d $b ]
+	then
+		dir[$cnt]=$b
+		fl[$cnt]=`find $b -type f | wc -l`
+		sf[$cnt]=0
+		for a in `find $b -type f -printf "%k "`
+		do
+			sf[$cnt]=$((${sf[$cnt]}+$a))
+			cnt=$(($cnt+1))
+		done
+	fi
+done
+
+max=0
+for ((p = 0; p < $cnt; p++))
+do
+	for ((j = 0; j < $cnt; j++))
+	do
+		if ((${sf[$j]} > ${sf[$max]}))
+		then
+			max=$j
+		fi
+	done
+	echo "${dir[$max]}	${sf[$max]}K"
+	p[$max]=-1
+done
+
+echo
+echo
+
+max=0
+for ((p = 0; p < $cnt; p++))
+do
+	for ((j = 0; j < $cnt; j++))
+	do
+		if ((${fl[$j]} > ${fl[$max]}))
+		then
+			max=$j
+		fi
+	done
+	echo "${dir[$max]}	${fl[$max]}"
+	fl[$max]=-1
+done
+
+
